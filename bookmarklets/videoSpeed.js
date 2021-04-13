0e52ebb61ec485dd2f8c063e6283497eb0adc487
Aaron Loo <admin@aaronloo.com>
2021-04-12 17:46:37 -0700

    input validation for videoSpeed bookmarklet

--- bookmarklets/videoSpeed.js
+++ bookmarklets/videoSpeed.js
@@ -3,6 +3,16 @@
  */
 (function() {
     const speed = prompt("Input desired speed (e.g. 1.5)");
+    if (!speed) {
+        //  Gracefully handle no input provided.
+        return;
+    }
+
+    if (Number.isNaN(Number.parseFloat(speed))) {
+        alert("Invalid speed input.");
+        return;
+    }
+
     document.getElementsByTagName("video").forEach((video) => {
         video.playbackRate = speed;
     });
